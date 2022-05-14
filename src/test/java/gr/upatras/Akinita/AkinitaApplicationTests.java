9fa481b40350bb501228e94b4e64d62a16286d9c
nickaris1 <issarisnick@outlook.com>
2022-05-14 20:48:17 +0300

    Init

---
+++ src/test/java/gr/upatras/Akinita/AkinitaApplicationTests.java
@@ -0,0 +1,13 @@
+package gr.upatras.Akinita;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AkinitaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
